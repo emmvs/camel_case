38b95f75b33f35376270880d30f49cecce4272ae
Emma Rünzel <emma@ruenzel.de>
2022-01-29 16:47:53 +0100

    validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,5 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :camel
+  validates :check_in, :check_out, :state, presence: true
 end
--- app/models/camel.rb
+++ app/models/camel.rb
@@ -1,3 +1,5 @@
 class Camel < ApplicationRecord
   belongs_to :user
+  has_many :bookings, dependent: :destroy
+  validates :name, :age, :description, :price, :city, :purpose, presence: true
 end
