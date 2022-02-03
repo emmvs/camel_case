651e9d9d8813a5a9fc7754e7480a34ccf8fb6d00
Dustin Dillenburg <dustin.dillenburg@gmail.com>
2022-02-03 22:53:51 +0100

    seeds have changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_205716) do
+ActiveRecord::Schema.define(version: 2022_02_03_201825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 2022_02_02_205716) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "sku"
     t.index ["user_id"], name: "index_camels_on_user_id"
   end
 
