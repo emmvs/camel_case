5a15a717dfb2ac23aa73f86391f9af1ee83fe000
Dustin Dillenburg <dustin.dillenburg@gmail.com>
2022-02-08 02:42:03 +0100

    merging master, fixing conflict issues

--- config/routes.rb
+++ config/routes.rb
@@ -10,18 +10,13 @@ Rails.application.routes.draw do
     resources :bookings, only: [ :new, :create ]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-<<<<<<< HEAD
-  resources :bookings, only: [ :edit, :update ]
 
   get "checkout/start/:id", to: "checkouts#start", as: "checkout"
   get "payment/start/:id", to: "payments#start", as: "payment"
 
   # get "/webhook", to: "webhooks#webhook"
   mount StripeEvent::Engine, at: '/stripe-webhooks'
-=======
+
   resources :bookings, only: [ :edit, :update, :index ]
   # get 'camels/index', to: "camels#index"
-
-
->>>>>>> master
 end
